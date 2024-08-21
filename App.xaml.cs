943733d2aaaa2e092451ef536f41e4f971d7812a
DanielRinconGam <danielrincongamboa@gmail.com>
2024-08-20 21:46:36 -0500

    Proyecto base

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GrassGol
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
