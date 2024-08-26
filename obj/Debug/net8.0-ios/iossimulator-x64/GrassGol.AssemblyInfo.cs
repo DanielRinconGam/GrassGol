fae33e13b1e705d7ea67e3fed219381e5a2253d3
DanielRinconGam <danielrincongamboa@gmail.com>
2024-08-26 14:07:19 -0500

    Cambio plataformas

--- obj/Debug/net8.0-ios/iossimulator-x64/GrassGol.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/GrassGol.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrassGol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+943733d2aaaa2e092451ef536f41e4f971d7812a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrassGol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrassGol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
