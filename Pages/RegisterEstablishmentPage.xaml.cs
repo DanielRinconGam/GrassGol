d4cf0115dba5eb422a321e857acc6961f6b60922
Neyder-Dev <naquemba@ucompensar.edu.co>
2024-10-08 10:37:13 -0500

    Encripta contrasenas, registro usuarios y empresas

--- Pages/RegisterEstablishmentPage.xaml.cs
+++ Pages/RegisterEstablishmentPage.xaml.cs
@@ -1,14 +1,52 @@
+using GrassGol.Connections;
+
 namespace GrassGol.Pages;
 
 public partial class RegisterEstablishmentPage : ContentPage
 {
-	public RegisterEstablishmentPage()
-	{
-		InitializeComponent();
-	}
+    private ConexionDB database;
+    private string _wname, _wlastname, _wemail, _wpass0;
+
+
+    public RegisterEstablishmentPage(string wname, string wlastname, string wemail, string wpass0)
+    {
+        InitializeComponent();
+        _wname = wname;
+        _wlastname = wlastname;
+        _wemail = wemail;
+        _wpass0 = wpass0;
+        database = new ConexionDB();
+    }
 
     private async void Button_Clicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("//businessHome", true);
+        string bname = establishmentNameEntry.Text;
+        string badress = establishmentAddressEntry.Text;
+        string btel = establishmentPhoneEntry.Text;
+        string bnit = establishmentNitEntry.Text;
+
+        bool nitfree = await database.NewNit(bnit);
+        if (!nitfree)
+        {
+            await DisplayAlert("Registro", $"{bnit} ya se encuentra registrado", "OK");
+            return;
+        }
+        int empresaId = await database.CreateBusiness(bname, badress, bnit, btel);
+        if (empresaId > 0) // Verificar si el ID es válido (empresa creada correctamente)
+        {
+            bool registerw = await database.RegisterWorkersAdmin(empresaId, _wname, _wlastname, _wemail, _wpass0, 1);
+            if (registerw)
+            {
+                await DisplayAlert("Registro", $"Establecimiento registrado con éxito! \n El administrador será {_wname}", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error", "No se pudo registrar el administrador", "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Error", "No se pudo registrar el establecimiento", "OK");
+        }
     }
 }
--- Pages/RegisterUserPage.xaml.cs
+++ Pages/RegisterUserPage.xaml.cs
@@ -1,29 +1,81 @@
 namespace GrassGol.Pages;
-
+using GrassGol.Connections;
+using GrassGol.Pages.Business;
 public partial class RegisterUserPage : ContentPage
 {
-	public RegisterUserPage()
+    private ConexionDB database;
+
+    public RegisterUserPage()
 	{
 		InitializeComponent();
-	}
+        database = new ConexionDB();
+    }
 
     private async void RegisterClicked(object sender, EventArgs e)
     {
-        if(radioUser.IsChecked)
+      
+        if (radioUser.IsChecked)
         {
-
+            string name = registerNameEntry.Text;
+            string lastname = registerLastnameEntry.Text;
+            string email = registerEmailEntry.Text;
+            string pass0 = registerPasswordEntry.Text;
+            string pass1 = registerRePasswordEntry.Text;
+            if (pass0 != pass1)
+            {
+                await DisplayAlert("Registro", "Las contraseñas no coinciden", "OK");
+                return;
+            }
+            else if (pass0.Length < 8)
+            {
+                await DisplayAlert("Registro", "La contraseña debe tener mínimo 8 caracteres", "OK");
+                return;
+            }
+            bool emailfree = await database.NewEmail(email);
+            if (!emailfree)
+            {
+                await DisplayAlert("Registro", $"{email} ya se encuentra registrado", "OK");
+                return;
+            }
+            bool register = await database.RegisterUser(name, lastname, email, pass0);
+            if (register)
+            {
+                await DisplayAlert("Registro", "Registrado con éxito!", "OK");
+                await Shell.Current.GoToAsync("//login", true); 
+            }
+            else
+            {
+                await DisplayAlert("Registro", "Hubo un error en el registro", "OK");
+            }
         }
         else if (radioEstablishment.IsChecked)
         {
-            await Shell.Current.GoToAsync("//establishment", true);
+            string wname = registerNameEntry.Text;
+            string wlastname = registerLastnameEntry.Text;
+            string wemail = registerEmailEntry.Text;
+            string wpass0 = registerPasswordEntry.Text;
+            string wpass1 = registerRePasswordEntry.Text;
+            if (wpass0 != wpass1)
+            {
+                await DisplayAlert("Registro", "Las contraseñas no coinciden", "OK");
+                return;
+            }
+            else if (wpass0.Length < 8)
+            {
+                await DisplayAlert("Registro", "La contraseña debe tener mínimo 8 caracteres", "OK");
+                return;
+            }
+            bool emailfree = await database.NewEmail(wemail);
+            if (!emailfree)
+            {
+                await DisplayAlert("Registro", $"{wemail} ya se encuentra registrado", "OK");
+                return;
+            }
+            await Navigation.PushAsync(new RegisterEstablishmentPage(wname, wlastname, wemail, wpass0));
         }
-
     }
-
     private async void LoginTapped(object sender, TappedEventArgs e)
     {
         await Shell.Current.GoToAsync("//login", true);
-    }
-
-    
+    }    
 }
